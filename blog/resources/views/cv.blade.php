c187d02ea80fc63805a44c809ee52ce940f75f5e
dylangouzenne <dylan.gouzenne@gmail.com>
2018-10-02 11:15:07 +0200

    initial commit

---
+++ blog/resources/views/cv.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CV Dylan</title>
+	<meta charset="utf-8">
+	<link href="{{ asset('/css/style2.css') }}" rel="stylesheet">
+	
+</head>
+<body>
+	
+	<div class="presentation-personelle">
+		
+		<h2 class="titre">Dylan Gouzenne</h2>
+
+		<div id="info-contact">
+
+			<div class="info-contact-box" id="identite">
+				
+				<p>Adresse : 32 place de l'hôtel de ville <br> 32230 Marciac</p>
+				
+				<p>Mail : contact@dylangouzenne.fr</p>
+
+				<p>Tel : 06.69.70.58.13</p>
+
+			</div>
+		
+			<div class="info-contact-box info-contact-box-photo">
+				<div id="photo-profil"></div>
+			</div>
+
+			<div class="info-contact-box" id="reseaux-sociaux">
+				<p>remplir lien vers réseaux sociaux linkedIn, github, portfolio dg.fr</p>
+			</div>
+
+		</div>
+
+		<h1 id="titre-poste">Etudiant dévelopeur web full stack junior</h1>
+
+	</div>
+
+		
+
+	<div class="competences">
+
+		
+		<h2 id="titre-competences">Compétences</h2>
+				
+
+		<div class="competences-box">
+
+			<div class="developpement box">
+
+				<h4 class="titre">Développement</h4>
+			
+				<p><b>Programmation :</b> HTML, CSS, javascript, PHP,</p>
+					
+				<p>Base de données : langage SQL </p>
+					
+				<p><b>Compétence Web :</b> installation et administration de serveur Web Apache<br>
+				CMS Wordpress, hébergement de site web.</p>
+
+			</div>
+
+			<div class="systeme box">
+				
+				<h4 class="titre">Système</h4>
+
+				<p><b>Bureautique :</b> Word, Excel, Internet, Access, PowerPoint</p>
+				
+				<p><b>Système d’exploitation :</b> Windows 7, 8 et 10 ; Windows Server 2008, 2012 et 2016 ; Mac OSX 9 et 10, Linux Fedora, CentOS et Ubuntu.</p>
+
+				<p><b>Gestion de parc :</b> mise en place DHCP, DNS, contrôleur de domaine.</p>
+
+				<p><b>Gestion d’incidents :</b> Création et Gestion de ticket d’incident. Création de documentation technique et de rapport d’incident.</p>
+
+			</div>
+
+			<div class="reseau box">
+				
+				<h4 class="titre">Réseau</h4>
+
+				<p>Connaissances Réseaux (TCP/IP), Modèle OSI.</p>
+
+				<p><b>Conception infra réseau :</b> Réalisation et mise en place de plan d’adressage IP.</p>
+				
+				<p><b>Administration réseau :</b> Configuration et administration de Switch et routeur Cisco Catalyst 2900 et 1800 Série.</p> 
+
+				<p><b>Sécurité réseau :</b> Configuration firewall et mise en place de règle de filtrage.</p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<div class="experiences-professionelle">
+
+		<h2 class="titre">Experiences-professionelle</h2>
+
+		<img class="logo-ecole" src="img/logo-spie.jpg">
+
+		<p>2016 – 2017 : Technicien AD niveau 1. SPIE-ICS
+		J’ai effectué mes stages de BTS SIO ainsi que de
+		l’intérim et pour finir un CDD.</p>
+		
+		<h4>Autres expériences</h4>
+		
+		<p>2014 - 2015 : J’ai effectué diverses missions
+		intérimaires pour le compte de l’agence AXE TT
+		d’Auch.</p>
+
+	</div>
+
+	<div class="formations">
+
+		<h2 class="titre">Formations</h2>
+
+		<div class="formations-box">
+
+			<div class="box">
+
+				<img class="logo-ecole" src="img/logo-simplon.png">
+
+				<p>30/05/18 – 30/04/19 : Formation ERN Simplon Auch Dévelopeur web. Ecole Simplon 32000 Auch.</p>
+
+			</div>
+			
+			<div class="box">
+
+				<img class="logo-ecole" src="img/logo-ece.jpg">
+
+				<p>28/09/2015 - 04/07/2017 : Formation BTS-SIO
+				(Service Informatique au Organisations) Option
+				Réseau. Ecole ECE-Tech campus Eiffel 75015 Paris.</p>
+
+			</div>
+
+			<div class="box">
+
+				<img class="logo-ecole" src="img/logo1-lycée.jpg">
+
+				<p>04/09/2010 - 04/07/2013 : Formation BAC-PRO-TP
+				(Travaux Public) Lycée Paul-Mathou 31210 Gourdan-
+				Polignan.</p>
+
+			</div>
+
+		</div>
+	</div>
+
+<script type="text/javascript"src="jquery.js"></script>
+<script type="text/javascript"src="cv2.js"></script>
+
+</body>
+</html>
